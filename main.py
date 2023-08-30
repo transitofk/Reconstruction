58022b9cd7e4717834206848d24f8de69167d809
transitofk <108511287+transitofk@users.noreply.github.com>
2023-08-30 10:50:48 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import cv2
-import numpy as np
-# import math
-# import matplotlib.pyplot as plt
-
-# from saving_results import save_book
-from counting_metrics import metrics_new
-from constructing_a_super_resolution import tests, reconstruction, reconstruction_primitive, choose_filter, add_noise
 from view import main_page
 
 
